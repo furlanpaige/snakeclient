577db1955752e79214cf3bb625a902c6db92ee0d
Paige <54337935+furlanpaige@users.noreply.github.com>
2022-03-25 23:45:29 -0700

    Added IP/HOST

--- play.js
+++ play.js
@@ -3,8 +3,8 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: // IP address here,
-    port: // PORT number here,
+    host: '127.0.0.1',
+    port: '50541',
   });
 
   // interpret incoming data as text
