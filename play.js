6000de6fa07d7bda13f39e61fb2af6d1760e8de6
Paige <54337935+furlanpaige@users.noreply.github.com>
2022-03-26 00:35:16 -0700

    Update error and synthesize code

--- play.js
+++ play.js
@@ -29,15 +29,12 @@ const setupInput = function () {
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-  
-};
-  
-const handleUserInput = function (data) {
+  stdin.on('data', (data) {
    if (data === '\u0003') {
      process.exit();
    } 
 };
+
+return stdin;
 
 setupInput();
