0cd0b827414bdb2be6f8759f19f15f90fccaf216
Paige <54337935+furlanpaige@users.noreply.github.com>
2022-03-26 00:17:20 -0700

    Fix Errors

--- play.js
+++ play.js
@@ -21,17 +21,17 @@ const connect = function () {
 console.log("Connecting ...");
 connect();
 
-
 const setupInput = function () {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
+  stdin.on('data', handleUserInput);
   return stdin;
+  
 };
-
-stdin.on("data", handleUserInput);
-const handleUserInput = function () {
+  
+const handleUserInput = function (data) {
    if (data === '\u0003') {
      process.exit();
    } 
