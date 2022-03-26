1e02cb0842f7c2915d33d23594a92e766a62457a
Paige <54337935+furlanpaige@users.noreply.github.com>
2022-03-25 23:53:16 -0700

    Move code to Client.js and input "Snake Initials"

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '127.0.0.1',
+    port: '50541',
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  
+   conn.on("connect", (data) => {
+      console.log("Server says: "data" );
+   
+});
+  
+    conn.on("connect", (name) => {
+      console.log("Name: PF");
+   });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
