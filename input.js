625e858f3440fba1204fd4654936ee57d5272ff6
Paige <54337935+furlanpaige@users.noreply.github.com>
2022-03-26 00:36:45 -0700

    Update input.js

--- input.js
+++ input.js
@@ -1,17 +1,37 @@
+let connection; 
+
+const setupInput = (conn) => {
+  connection = conn;
+};
+
 const setupInput = function () {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-  
-};
-  
-const handleUserInput = function (data) {
+  stdin.on('data', (Data) => {
    if (data === '\u0003') {
      process.exit();
    } 
-};
+    connection.write("Say: Hi Paige!")
+    if (data === 'w') {
+      console.log("Move: up");
+      connection.write("Move: up");
+    }
+    if (data === 'a') {
+      console.log("Move: left");
+      connection.write("Move: left");
+    }
+    if (data === 's') {
+      console.log("Move: down");
+      connection.write("Move: down");
+    }
+    if (data === 'd') {
+      console.log("Move: right");
+      connection.write("Move: right");
+    }
+  });
+  
+  return stdin;
 
-setupInput();
+setupInput(connection);
